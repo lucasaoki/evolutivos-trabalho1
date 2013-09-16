debd2c66c4c709222dc07ff84c8f1c479288e112
seiji.ishibe <seiji.ishibe@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-09-16 19:01:38 +0000

    Add - Funções

---
+++ ProOFJava/src/ProOF/apl/problems/EXP.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProOF.apl.problems;
+
+import ProOF.apl.methods.Cod;
+import ProOF.opt.abst.problem.meta.Problem;
+import ProOF.opt.abst.problem.meta.codification.Codification;
+import ProOF.opt.abst.problem.meta.objective.SingleObjective;
+
+/**
+ *
+ * @author Seiji
+ */
+public class EXP extends SingleObjective<Problem, Codification, SingleObjective>{
+    private double _max;
+    private double _min;
+    
+    public EXP()throws Exception{
+        //this._max = 1;
+        //this._min = -1;
+    }
+ 
+    public double getMax(){
+        return _max;
+    }
+    
+    public double getMin(){
+        return _min;
+    }
+    
+    @Override
+    public SingleObjective New(Problem prob) throws Exception {
+        return new EXP();
+    }
+    
+    @Override
+    public void Evaluate(Problem prob, Codification codif) throws Exception {
+        double[] tmp = ((Cod) codif).getInd();
+        double sum = 0;
+        double eval;
+        int i;
+        int size = ((Cod) codif).getSize();
+
+        for(i = 0 ; i < size ; i++){
+            sum += Math.sqrt(tmp[i]);
+        }
+        eval = Math.exp(-0.5*sum);
+        ((Cod) codif).setFitness(eval);
+    } 
+}
