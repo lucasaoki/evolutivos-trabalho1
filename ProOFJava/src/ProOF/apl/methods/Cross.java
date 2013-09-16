d2f6ecd78c7595ff7e5bf9d4171150508d6b0f21
aoki.ec.usp@gmail.com <aoki.ec.usp@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-09-16 00:45:34 +0000

    Crossover ready (maybe?)

--- ProOFJava/src/ProOF/apl/methods/Cross.java
+++ ProOFJava/src/ProOF/apl/methods/Cross.java
@@ -7,6 +7,7 @@ package ProOF.apl.methods;
 import ProOF.gen.operator.oCrossover;
 import ProOF.opt.abst.problem.meta.Problem;
 import ProOF.opt.abst.problem.meta.codification.Codification;
+import java.util.Random;
 
 /**
  *
@@ -14,14 +15,96 @@ import ProOF.opt.abst.problem.meta.codification.Codification;
  */
 public class Cross extends oCrossover {
 
+    private int size;
+    private double fitness;
+    private Cod newInd;
+
     @Override
     public Codification crossover(Problem mem, Codification ind1, Codification ind2) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        size = ((Cod) ind1).getSize();
+        newInd = new Cod(size);
+        /* Escolhe um metodo aleatorio */
+        switch ((new Random(0).nextInt(3))) {
+            case 0:
+                method1((Cod) ind1, (Cod) ind2);
+                break;
+            case 1:
+                method2((Cod) ind1, (Cod) ind2);
+                break;
+            case 2:
+                method3((Cod) ind1, (Cod) ind2);
+                break;
+        }
+        /* Calculo do Fitness */
+        ((Prob) mem).ob.Evaluate(mem, newInd);
+
+        return newInd;
     }
 
     @Override
     public String name() {
         return "Cross";
     }
-    
+
+    /**
+     * 50% do ind1 e 50% do ind2
+     */
+    private void method1(Cod ind1, Cod ind2) {
+
+        for (int i = 0; i < size; i++) {
+            /* Metade de cada */
+            if (i < size / 2) {
+                newInd.setIndVal(ind1.getIndVal(i), i);
+            } else {
+                newInd.setIndVal(ind2.getIndVal(i), i);
+            }
+        }
+    }
+
+    /**
+     * Intercalação
+     */
+    private void method2(Cod ind1, Cod ind2) {
+
+        for (int i = 0; i < size; i++) {
+            /* Intercalando */
+            if (i % 2 == 0) {
+                newInd.setIndVal(ind1.getIndVal(i), i);
+            } else {
+                newInd.setIndVal(ind2.getIndVal(i), i);
+            }
+        }
+    }
+
+    /**
+     * Ponderada dos melhores valores (está implementada para minimização
+     */
+    private void method3(Cod ind1, Cod ind2) {
+
+        Cod bestTmp, worstTmp;
+        double fitTmp;
+
+        /* Pegando menor valor */
+        if (ind1.getFitness() < ind2.getFitness()) {
+            bestTmp = ind1;
+            worstTmp = ind2;
+        } else {
+            bestTmp = ind2;
+            worstTmp = ind1;
+        }
+        fitTmp = bestTmp.getFitness() / (ind1.getFitness() + ind2.getFitness());
+
+        if (Math.random() > fitTmp) {
+            /* Melhor individuo */
+            for (int i = 0; i < bestTmp.getSize(); i++) {
+                newInd.setIndVal(bestTmp.getIndVal(i), i);
+            }
+        } else {
+            /* Pior individuo */
+            for (int i = 0; i < worstTmp.getSize(); i++) {
+                newInd.setIndVal(worstTmp.getIndVal(i), i);
+            }
+        }
+    }
 }
