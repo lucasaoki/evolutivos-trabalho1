0cb4ba049f008f05106d26711b1aefce88301aa3
aoki.ec.usp@gmail.com <aoki.ec.usp@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-09-25 03:16:46 +0000

    Correcao de uma string

--- ProOFJavaAlternative/src/ProOF/apl/problems/functions/iFunctionEXP.java
+++ ProOFJavaAlternative/src/ProOF/apl/problems/functions/iFunctionEXP.java
@@ -14,30 +14,30 @@ public class iFunctionEXP extends aFunction {
 
     @Override
     public String name() {
-	return "CM Function";
+        return "EXP Function";
     }
 
     @Override
     public String description() {
-	return "To be done (CM Function)";
+        return "To be done (EXP Function)";
     }
 
     public iFunctionEXP() {
-	//IMPORTANT!!! SUPER(SIZE)
-	super(10);
-	for (int i = 0; i < limitList.size(); i++) {
-	    limitList.get(i).setLimits(-1, 1);
-	}
+        //IMPORTANT!!! SUPER(SIZE)
+        super(10);
+        for (int i = 0; i < limitList.size(); i++) {
+            limitList.get(i).setLimits(-1, 1);
+        }
     }
 
     @Override
     public double Evaluate(iCodification codif) throws Exception {
-	double[] x = codif.getInd();
-	double sum = 0;
+        double[] x = codif.getInd();
+        double sum = 0;
 
-	for (int i = 0; i < codif.getSize(); i++) {
-            sum += Math.pow(x[i],2);
+        for (int i = 0; i < codif.getSize(); i++) {
+            sum += Math.pow(x[i], 2);
         }
-	return (-Math.exp(-0.5 * sum));
+        return (-Math.exp(-0.5 * sum));
     }
 }
