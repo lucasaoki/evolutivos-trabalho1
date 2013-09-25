f0ff3afd73ad1b8506d1a0625c95898d60cf3e37
fernandosci@gmail.com <fernandosci@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-09-25 01:37:05 +0000

    fix compiling on framework

--- trunk/ProOFJavaAlternative/src/ProOF/apl/problems/operators/mutation/algorithm/iMutationInv.java
+++ trunk/ProOFJavaAlternative/src/ProOF/apl/problems/operators/mutation/algorithm/iMutationInv.java
@@ -7,7 +7,6 @@ package ProOF.apl.problems.operators.mutation.algorithm;
 import ProOF.apl.problems.iCodification;
 import ProOF.apl.problems.iProblem;
 import ProOF.gen.operator.oMutation;
-import org.apache.commons.lang3.ArrayUtils;
 
 /**
  *
@@ -17,7 +16,17 @@ public class iMutationInv extends oMutation<iProblem, iCodification> {
 
     @Override
     public void mutation(iProblem mem, iCodification ind) throws Exception {
-	ArrayUtils.reverse(ind.getInd());
+
+	System.out.print("\nBEFORE:\n");
+	for (double d : ind.getInd()) {
+	    System.out.print(Double.toString(d) + " ");
+	}
+
+	//ArrayUtils.reverse(ind.getInd());
+	System.out.print("\nAFTER:\n");
+	for (double d : ind.getInd()) {
+	    System.out.print(Double.toString(d) + " ");
+	}
 
     }
 
