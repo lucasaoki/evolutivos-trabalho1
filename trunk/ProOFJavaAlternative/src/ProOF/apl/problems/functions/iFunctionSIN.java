5065ef9884b279e4c240535335ab0b4f0ae78f50
lukehgp@gmail.com <lukehgp@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-09-25 02:49:42 +0000

    MR, SIN and WP Functions Implementations

--- trunk/ProOFJavaAlternative/src/ProOF/apl/problems/functions/iFunctionSIN.java
+++ trunk/ProOFJavaAlternative/src/ProOF/apl/problems/functions/iFunctionSIN.java
@@ -4,10 +4,50 @@
  */
 package ProOF.apl.problems.functions;
 
+import ProOF.apl.problems.iCodification;
+
 /**
  *
  * @author Lucas
  */
-public class iFunctionSIN {
-    
+public class iFunctionSIN extends aFunction {
+
+    private double A = 2.5;
+    private double B = 5;
+    private double z = 30;
+
+    public iFunctionSIN() {
+        super(20);
+        for (int i = 0; i < 20; i++) {
+            limitList.get(i).setLimits(0.0, 180.0);
+        }
+
+    }
+
+    @Override
+    public double Evaluate(iCodification codif) throws Exception {
+        double[] x = codif.getInd();
+        double eval;
+
+        double prod1 = 0;
+        double prod2 = 0;
+        int i;
+        for (i = 0; i < 20; i++) {
+            prod1 *= Math.sin(x[i] - z);
+            prod2 *= Math.sin(B * (x[i] - z));
+        }
+        eval = -(A * prod1 + prod2);
+
+        return eval;
+    }
+
+    @Override
+    public String name() {
+        return "SIN Function";
+    }
+
+    @Override
+    public String description() {
+        return "To be done (SIN Function)";
+    }
 }
--- trunk/ProOFJavaAlternative/src/ProOF/apl/problems/functions/iFunctionWP.java
+++ trunk/ProOFJavaAlternative/src/ProOF/apl/problems/functions/iFunctionWP.java
@@ -4,10 +4,41 @@
  */
 package ProOF.apl.problems.functions;
 
+import ProOF.apl.problems.iCodification;
+
 /**
  *
  * @author Lucas
  */
-public class iFunctionWP {
+public class iFunctionWP extends aFunction {
+
+    public iFunctionWP(){
+        super(4);
+        limitList.get(0).setLimits(-10.0, 10.0);
+        limitList.get(1).setLimits(-10.0, 10.0);
+        limitList.get(2).setLimits(-10.0, 10.0);
+        limitList.get(3).setLimits(-10.0, 10.0);
+    }
+    
+    
+    @Override
+    public double Evaluate(iCodification codif) throws Exception {
+        double[] x = codif.getInd();
+        double eval;
+        eval = 100 * Math.pow(x[1] - Math.pow(x[0],2),2) + Math.pow(1 - x[0],2) + 90 * Math.pow(x[3] - Math.pow(x[2],2),2) + Math.pow(1 - x[2],2);
+        eval += 10.1 * (Math.pow(x[1] - 1,2) + Math.pow(x[3] - 1,2)) + 19.8 * (x[1] - 1) * (x[3] - 1);
+        
+        return eval;
+    }
+
+    @Override
+    public String name() {
+        return "WP Function";
+    }
+
+    @Override
+    public String description() {
+        return "To be done (WP Function)";
+    }
     
 }
