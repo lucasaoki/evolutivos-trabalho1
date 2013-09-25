4ee819de49591c96a35c2656934d3de69fb2a0d4
fernandosci@gmail.com <fernandosci@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-09-25 03:19:48 +0000

    removed prints

--- ProOFJavaAlternative/src/ProOF/apl/problems/operators/mutation/algorithm/iMutationInv.java
+++ ProOFJavaAlternative/src/ProOF/apl/problems/operators/mutation/algorithm/iMutationInv.java
@@ -17,16 +17,7 @@ public class iMutationInv extends oMutation<iProblem, iCodification> {
 
     @Override
     public void mutation(iProblem mem, iCodification ind) throws Exception {
-	System.out.print("\nBEFORE:\n");
-	for (double d : ind.getInd()) {
-	    System.out.print(Double.toString(d) + " ");
-	}
-
 	ArrayUtils.reverse(ind.getInd());
-	System.out.print("\nAFTER:\n");
-	for (double d : ind.getInd()) {
-	    System.out.print(Double.toString(d) + " ");
-	}
     }
 
     @Override
