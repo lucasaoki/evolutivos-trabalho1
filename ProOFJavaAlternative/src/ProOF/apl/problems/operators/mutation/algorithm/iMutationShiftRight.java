b03f8fe96be7affd180e6665db5e5ba3e15fde82
seiji.ishibe@gmail.com <seiji.ishibe@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-09-26 01:14:54 +0000

    Mutation.....v8

---
+++ ProOFJavaAlternative/src/ProOF/apl/problems/operators/mutation/algorithm/iMutationShiftRight.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProOF.apl.problems.operators.mutation.algorithm;
+
+import ProOF.apl.problems.functions.aFunction;
+import ProOF.apl.problems.iCodification;
+import ProOF.apl.problems.iProblem;
+import ProOF.gen.operator.oMutation;
+
+/**
+ *
+ * @author Seiji
+ */
+public class iMutationShiftRight extends oMutation<iProblem, iCodification> {
+
+    @Override
+    public void mutation(iProblem mem, iCodification ind) throws Exception {
+        aFunction ifunction = mem.getIFunc();
+        for (int i = 0; i < ind.getSize(); i++) {
+            double value = ind.getIndVal(i);
+            double max = ifunction.getMax(i);
+            double min = ifunction.getMin(i);
+            double per = 0.01;
+            if (value - min < max - value) {
+                if (value - min < (max - min) / 8) {
+                    per += 0.99;
+                }
+                if (Math.random() < 0.4) {
+                    ind.setIndVal(value - (value - min) * Math.random() * per, i);
+                } else {
+                    ind.setIndVal(value + (value - min) * Math.random() * per, i);
+                }
+            } else {
+                if (max - value < (max - min) / 8) {
+                    per += 0.99;
+                }
+                if (Math.random() < 0.4) {
+                    ind.setIndVal(value + (max - value) * Math.random() * per, i);
+                } else {
+                    ind.setIndVal(value - (max - value) * Math.random() * per, i);
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public String name() {
+        return "Mut Shift Left";
+    }
+}
