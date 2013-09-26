90eec64677b3cc67b173e1b5afc711d6bbf40a4a
lukehgp@gmail.com <lukehgp@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-09-26 05:06:56 +0000

    Mutations Zuadas do Luiz Fix - Limits Respected

--- trunk/ProOFJavaAlternative/src/ProOF/apl/problems/operators/mutation/algorithm/iMutationSwapChance.java
+++ trunk/ProOFJavaAlternative/src/ProOF/apl/problems/operators/mutation/algorithm/iMutationSwapChance.java
@@ -12,27 +12,28 @@ import ProOF.gen.operator.oMutation;
  *
  * @author Luke
  */
-public class iMutationSwapChance extends oMutation<iProblem, iCodification>{
+public class iMutationSwapChance extends oMutation<iProblem, iCodification> {
 
     @Override
     public void mutation(iProblem mem, iCodification ind) throws Exception {
         for (int i = 0; i < ind.getSize(); i++) {
-                    double rand = Math.random();
-                    if (rand < 0.6) {
-                        int p1 = i;
-                        int p2 = 0 + (int) (Math.random() * (ind.getSize() + 0));
+            double rand = Math.random();
+            if (rand < 0.6) {
+                int p1 = i;
+                int p2 = 0 + (int) (Math.random() * (ind.getSize() + 0));
+                if (!(ind.getIndVal(p1) < mem.getIFunc().getMin(p2) || ind.getIndVal(p1) > mem.getIFunc().getMax(p2))
+                        && !(ind.getIndVal(p2) < mem.getIFunc().getMin(p1) || ind.getIndVal(p2) > mem.getIFunc().getMax(p1))) {
 
-                        double tmp = ind.getIndVal(p1);
-                        ind.setIndVal(ind.getIndVal(p2), p1);
-                        ind.setIndVal(tmp, p2);
-                    }
+                    double tmp = ind.getIndVal(p1);
+                    ind.setIndVal(ind.getIndVal(p2), p1);
+                    ind.setIndVal(tmp, p2);
                 }
+            }
+        }
     }
 
     @Override
     public String name() {
         return "Mut Swap Chance";
     }
-
-    
 }
