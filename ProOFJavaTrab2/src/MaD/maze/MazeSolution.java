a4359d76709c18761fceaccad564b20e4a0f4e66
fernandosci@gmail.com <fernandosci@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-11-20 23:36:00 +0000

    commit

--- ProOFJavaTrab2/src/MaD/maze/MazeSolution.java
+++ ProOFJavaTrab2/src/MaD/maze/MazeSolution.java
@@ -6,10 +6,54 @@
 
 package MaD.maze;
 
+import MaD.maze.components.MazeVertex;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author ito
  */
 public class MazeSolution {
+    private List<MazeVertex> vertexList;
+    
+    
+    private MazeVertex startVertex;
+    
+    private MazeVertex endVertex;
+
+    public MazeSolution(MazeVertex startVertex, MazeVertex endVertex) {
+        this.vertexList = new ArrayList<>();
+        this.startVertex = startVertex;
+        this.endVertex = endVertex;
+    }
+
+    public MazeVertex getStartVertex() {
+        return startVertex;
+    }
+
+    public void setStartVertex(MazeVertex startVertex) {
+        this.startVertex = startVertex;
+    }
+
+    public MazeVertex getEndVertex() {
+        return endVertex;
+    }
+
+    public void setEndVertex(MazeVertex endVertex) {
+        this.endVertex = endVertex;
+    }
+
+    public List<MazeVertex> getVertexList() {
+        return vertexList;
+    }
+
+    
+    
+ 
+    
+    
+    
+    
     
 }
