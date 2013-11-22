d4ac4ed3c4458b22d74fbea9f3879c5e015af14b
fernandosci@gmail.com <fernandosci@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-11-22 07:10:23 +0000

    commit

--- trunk/ProOFJavaTrab2/src/ProOF/utils/GlobalConstants.java
+++ trunk/ProOFJavaTrab2/src/ProOF/utils/GlobalConstants.java
@@ -14,6 +14,13 @@ public class GlobalConstants {
     
     public final static int limitIndividualSize = 10000;
     public final static int startIndividualSize = 5000;
+    
+    public final static int vertexDefaultStartIndex = 0;
+    public final static int vertexDefaultEndIndex = 0;
+    
+
+    
+    
 
     //aGA
     public final static int population_size = 300;
