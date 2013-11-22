088bbbd2f96fad64978e7bd28326894695a0e1a0
fernandosci@gmail.com <fernandosci@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-11-22 11:51:00 +0000

    commit

--- trunk/ProOFJavaTrab2/src/ProOF/apl/problems/operators/crossover/algorithm/iCrossoverDefault.java
+++ trunk/ProOFJavaTrab2/src/ProOF/apl/problems/operators/crossover/algorithm/iCrossoverDefault.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ProOF.apl.problems.operators.crossover.algorithm;
 
 import ProOF.MaD.maze.MazeSolution;
@@ -18,36 +17,32 @@ import java.util.HashMap;
  *
  * @author ito
  */
-public class iCrossoverDefault extends aCrossover{
+public class iCrossoverDefault extends aCrossover {
 
     @Override
     public iCodification crossover(iProblem mem, iCodification ind1, iCodification ind2) throws Exception {
-        
-       
-        if (mem.isUsingVertex())
-        {
-            HashMap<MazeVertex,Integer> vertex = new HashMap<>();
+
+        if (mem.isUsingVertex()) {
+            HashMap<MazeVertex, Integer> vertex = new HashMap<>();
             ArrayList<Integer> vind1Index = new ArrayList<>();
             ArrayList<Integer> vind2Index = new ArrayList<>();
-            
+
             MazeSolution mazeSol = ind1.getMazeSol();
-            
-            for (int c = 0; c< mazeSol.getSize(); c++)
-            {
-                 vertex.put(mazeSol.getVertexAt(c), c);
+
+            for (int c = 0; c < mazeSol.getSize(); c++) {
+                vertex.put(mazeSol.getVertexAt(c), c);
             }
-             mazeSol = ind2.getMazeSol();
-             
-             for (int c = 0; c< mazeSol.getSize(); c++)
-            {
-                if (vertex.containsKey(mazeSol.getVertexAt(c)))
-                {
+            mazeSol = ind2.getMazeSol();
+
+            for (int c = 0; c < mazeSol.getSize(); c++) {
+                if (vertex.containsKey(mazeSol.getVertexAt(c))) {
                     vind1Index.add(vertex.get(mazeSol.getVertexAt(c)));
                     vind2Index.add(c);
                 }
             }
+
         }
-       
+
         return (iCodification) mem.NewCodification();
     }
 
@@ -55,8 +50,5 @@ public class iCrossoverDefault extends aCrossover{
     public String name() {
         return "DefaultCrossover";
     }
-   
-    
-    
-    
+
 }
