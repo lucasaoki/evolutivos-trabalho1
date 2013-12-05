2784c4684e06f30d02514dbe4bbe96c34152ed63
seiji.ishibe@gmail.com <seiji.ishibe@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-12-05 17:56:01 +0000

    .....v20

--- ProOFJavaTrab2/src/PrintImage/Screen.java
+++ ProOFJavaTrab2/src/PrintImage/Screen.java
@@ -63,32 +63,53 @@ public class Screen extends JPanel {
 
         max_x = img.getWidth();
         max_y = img.getHeight();
-
         g2d.setColor(Color.LIGHT_GRAY);
         for (MazeEdge edge : edges) {
             MazeVertex vertex = maze.getEdgeSource(edge);
             MazeVertex vertex2 = maze.getEdgeTarget(edge);
-            g2d.drawLine((int) ((vertex.getX() * screen_width / max_x)), (int) ((vertex.getY() * screen_height / max_y)), (int) ((vertex2.getX() * screen_width / max_x)), (int) ((vertex2.getY() * screen_height / max_y)));
+            int pos_x = (int) ((vertex.getX() * screen_width / max_x));
+            int pos_y = (int) ((vertex.getY() * screen_height / max_y));
+            int pos_x1 = (int) ((vertex2.getX() * screen_width / max_x));
+            int pos_y1 = (int) ((vertex2.getY() * screen_height / max_y));
+            g2d.drawLine( pos_x ,pos_y , pos_x1, pos_y1);
         }
 
         g2d.setColor(Color.red);
         for (MazeVertex vertex : vertices) {
-            g2d.drawString("" + vertex.getIndex() + "", (int) ((vertex.getX() * screen_width / max_x)-4), (int) ((vertex.getY() * screen_height / max_y)-10));
+            int pos_x = (int) ((vertex.getX() * screen_width / max_x));
+            int pos_y = (int) ((vertex.getY() * screen_height / max_y));
+            g2d.drawString("" + vertex.getIndex() + "", (pos_x-4), (pos_y-10));
             g2d.fillOval((int) ((vertex.getX()* screen_width / max_x)-4), (int) ((vertex.getY() * screen_height / max_y)-4), 8, 8);
         }
         if(mazeS != null){
-            double angle  = 30;
+            double angle  = 90;
             double[] contador = new double[mazeS.getSize()];
             for(int c = 0 ; c < mazeS.getSize(); c++){
                 contador[c] = 0;
             }
             for (int c = 0; c < mazeS.getSize() - 1; c++) {
-                MazeVertex vertexAt = mazeS.getVertexAt(c);
-                MazeVertex vertexAt1 = mazeS.getVertexAt(c + 1);
+                MazeVertex vertex = mazeS.getVertexAt(c);
+                MazeVertex vertex1 = mazeS.getVertexAt(c + 1);
+                int pos_x = (int) ((vertex.getX() * screen_width / max_x));
+                int pos_y = (int) ((vertex.getY() * screen_height / max_y));
+                int pos_x1 = (int) ((vertex1.getX() * screen_width / max_x));
+                int pos_y1 = (int) ((vertex1.getY() * screen_height / max_y));
+                
+                if(c == mazeS.getSize() - 1){
+                    g2d.setColor(Color.ORANGE);
+                    g2d.fillOval(pos_x1-4, pos_y1-4, 10, 10);
+                }
+                else{
+                    if(c == 0){
+                        g2d.setColor(Color.magenta);
+                        g2d.fillOval(pos_x-4, pos_y-4, 10, 10);
+                    }
+                }
                 g2d.setColor(Color.green);
-                g2d.drawLine((int)((vertexAt.getX()* screen_width / max_x)), (int) ((vertexAt.getY()* screen_height / max_y)), (int) ((vertexAt1.getX()* screen_width / max_x)), (int) ((vertexAt1.getY()* screen_height / max_y)));
-                g2d.drawString("" + c + "", (int) (((vertexAt.getX() * screen_width / max_x)-4)+10*Math.cos(Math.toRadians( contador[vertexAt.getIndex()]*angle))), (int) (((vertexAt.getY() * screen_height / max_y)-10)+10*Math.cos(Math.toRadians( contador[vertexAt.getIndex()]*angle))));
-                contador[vertexAt.getIndex()]++;
+                g2d.drawLine(pos_x, pos_y, pos_x1, pos_y1);  
+                double pos_angle = 15*Math.cos(Math.toRadians( contador[vertex.getIndex()]*angle));
+                g2d.drawString("" + c + "", (int) ((pos_x-4) + pos_angle), (int) ((pos_y-10) - pos_angle));
+                contador[vertex.getIndex()]++;
             }
         }
     }
