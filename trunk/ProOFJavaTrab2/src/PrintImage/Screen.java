be62bd99dc27d6c071d1cf82aa6aa62205633a98
seiji.ishibe@gmail.com <seiji.ishibe@gmail.com@f3e333df-c83a-a0d5-b112-f38b342858aa>
2013-12-05 20:11:19 +0000

    .....v25

--- trunk/ProOFJavaTrab2/src/PrintImage/Screen.java
+++ trunk/ProOFJavaTrab2/src/PrintImage/Screen.java
@@ -18,8 +18,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 import java.awt.Dimension;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsEnvironment;
+import java.awt.Font;
 import java.awt.RenderingHints;
 import java.awt.Transparency;
 
@@ -58,37 +57,42 @@ public class Screen extends JPanel {
 
     public void Draw(Graphics2D g) {
         Graphics2D g2d = (Graphics2D) g;
-
         double max_x, max_y;
         g2d.setColor(Color.red);
         ArrayList<MazeVertex> vertices = maze.getVertices();
         Set<MazeEdge> edges = maze.edgeSet();
-
+        
         max_x = img.getWidth();
         max_y = img.getHeight();
 
+        g2d.setFont(new Font("TimesRoman", Font.BOLD, 10)); 
+        
         BufferedImage tmpImage = getGraphicsConfiguration().createCompatibleImage(screen_width, screen_height, Transparency.TRANSLUCENT);
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
                 RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.drawImage(img, 0, 0, screen_width, screen_height, null);
 
-        g2d.setColor(Color.LIGHT_GRAY);
         for (MazeEdge edge : edges) {
             MazeVertex vertex = maze.getEdgeSource(edge);
             MazeVertex vertex2 = maze.getEdgeTarget(edge);
+
             int pos_x = (int) ((vertex.getX() * screen_width / max_x));
             int pos_y = (int) ((vertex.getY() * screen_height / max_y));
             int pos_x1 = (int) ((vertex2.getX() * screen_width / max_x));
             int pos_y1 = (int) ((vertex2.getY() * screen_height / max_y));
+
+            g2d.setColor(Color.LIGHT_GRAY);
             g2d.drawLine(pos_x, pos_y, pos_x1, pos_y1);
         }
 
         for (MazeVertex vertex : vertices) {
             int pos_x = (int) ((vertex.getX() * screen_width / max_x));
             int pos_y = (int) ((vertex.getY() * screen_height / max_y));
+
             g2d.setColor(Color.magenta);
             g2d.fillOval((int) (pos_x - 4), (int) (pos_y - 4), 8, 8);
+
             g2d.setColor(Color.blue);
             g2d.drawString("" + vertex.getIndex() + "", (pos_x - 4), (pos_y - 5));
         }
@@ -98,9 +102,29 @@ public class Screen extends JPanel {
             for (int c = 0; c < vertices.size(); c++) {
                 contador[c] = 0;
             }
+
+            MazeVertex vert_inicial = maze.getStartVert();
+            MazeVertex vert_end = maze.getEndVert();
+            int initial_x = (int) ((vert_inicial.getX() * screen_width / max_x));
+            int initial_y = (int) ((vert_inicial.getY() * screen_height / max_y));
+            int end_x = (int) ((vert_end.getX() * screen_width / max_x));
+            int end_y = (int) ((vert_end.getY() * screen_height / max_y));
+            int aux_x = (int) ((mazeS.getVertexAt(0).getX() * screen_width / max_x));
+            int aux_y = (int) ((mazeS.getVertexAt(0).getY() * screen_height / max_y));
+
+            g2d.setColor(Color.blue);
+            g2d.fillOval(initial_x - 4, initial_y - 4, 10, 10);
+
+            g2d.setColor(Color.yellow);
+            g2d.fillOval(end_x - 4, end_y - 4, 10, 10);
+
+            g2d.setColor(Color.green);
+            g2d.drawLine(initial_x, initial_y, aux_x, aux_y);
+
             for (int c = 0; c < mazeS.getSize() - 1; c++) {
                 MazeVertex vertex = mazeS.getVertexAt(c);
                 MazeVertex vertex1 = mazeS.getVertexAt(c + 1);
+
                 int pos_x = (int) ((vertex.getX() * screen_width / max_x));
                 int pos_y = (int) ((vertex.getY() * screen_height / max_y));
                 int pos_x1 = (int) ((vertex1.getX() * screen_width / max_x));
@@ -109,16 +133,14 @@ public class Screen extends JPanel {
                 if (c == mazeS.getSize() - 2) {
                     g2d.setColor(Color.orange);
                     g2d.fillOval(pos_x1 - 4, pos_y1 - 4, 10, 10);
-                } else {
-                    if (c == 0) {
-                        g2d.setColor(Color.blue);
-                        g2d.fillOval(pos_x - 4, pos_y - 4, 10, 10);
-                    }
                 }
+
                 g2d.setColor(Color.green);
                 g2d.drawLine(pos_x, pos_y, pos_x1, pos_y1);
+
                 double pos_angle_cos = 10 * Math.cos(Math.toRadians(contador[vertex.getIndex()] * angle));
                 double pos_angle_sin = 10 * Math.sin(Math.toRadians(contador[vertex.getIndex()] * angle));
+
                 g2d.drawString("" + c + "", (int) ((pos_x - 4) + pos_angle_cos), (int) ((pos_y - 10) + pos_angle_sin));
                 contador[vertex.getIndex()]++;
             }
